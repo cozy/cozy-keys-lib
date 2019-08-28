965e34e04372345a6d23d2702873f0ee522eea87
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-28 15:30:18 +0200

    chore: Linting

--- playgrounds/src/cozy-keys.jsx
+++ playgrounds/src/cozy-keys.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Route } from 'react-router'
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import App from './common/App'
-import client from './common/client'
-import 'cozy-ui/transpiled/react/stylesheet.css'
-import { Sprite } from 'cozy-ui/transpiled/react/Icon'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route } from "react-router";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import App from "./common/App";
+import client from "./common/client";
+import "cozy-ui/transpiled/react/stylesheet.css";
+import { Sprite } from "cozy-ui/transpiled/react/Icon";
 //import 'cozy-ui/dist/cozy-ui.min.css'
-import { withClient } from 'cozy-client'
+import { withClient } from "cozy-client";
 
-import { VaultProvider, VaultUnlocker } from '../../transpiled'
+import { VaultProvider, VaultUnlocker } from "../../transpiled";
 
-
-let localConfig = { }
+let localConfig = {};
 try {
-  localConfig = require('../local.config.json')
-} catch(e) {
+  localConfig = require("../local.config.json");
+} catch (e) {
   // nothing
 }
 
 const reducer = combineReducers({
   cozy: client.reducer()
-})
+});
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 function VaultComponent({ client }) {
-  const uri = client.getStackClient().uri
-  const cfg = localConfig.keysInstance
+  const uri = client.getStackClient().uri;
+  const cfg = localConfig.keysInstance;
   return (
     <VaultProvider instance={cfg || uri}>
       <Sprite />
@@ -36,15 +35,14 @@ function VaultComponent({ client }) {
         <div>Unlocked</div>
       </VaultUnlocker>
     </VaultProvider>
-  )
+  );
 }
 
-
-const VaultWithClient = withClient(VaultComponent)
+const VaultWithClient = withClient(VaultComponent);
 
 ReactDOM.render(
   <App client={client} existingStore={store}>
     <Route path="/" component={VaultWithClient} />
   </App>,
-  document.querySelector('#app')
-)
+  document.querySelector("#app")
+);
