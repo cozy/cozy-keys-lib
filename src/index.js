167e3b8c7f41ae1816f5990f96fc6ef891ab1690
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-29 16:29:30 +0200

    feat: Export withVaultClient HOC

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export { WebVaultClient } from './WebVaultClient'
 export { CozyUtils } from './CozyUtils'
-export { VaultContext, VaultProvider } from './components/VaultContext'
+export {
+  VaultContext,
+  VaultProvider,
+  withVaultClient
+} from './components/VaultContext'
 export VaultUnlocker from './components/VaultUnlocker'
