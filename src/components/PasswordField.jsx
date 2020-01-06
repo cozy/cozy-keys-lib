fab305adca55beb632632856d94de90f211521df
Cyrille Perois <cperois@outlook.com>
2020-01-06 11:44:09 +0100

    fix(PasswordField): Add label on input adornment
    This caused a prop types warning as the label prop is required on
    cozy-ui's button

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import TextField from 'cozy-ui/transpiled/react/MuiCozyTheme/TextField'
 import Button from 'cozy-ui/transpiled/react/Button'
 import InputAdornment from '@material-ui/core/InputAdornment'
+import { translate } from 'cozy-ui/transpiled/react/I18n'
+import omit from 'lodash/omit'
 
-const EyeAdornment = ({ hidden, ...props }) => {
+const DumbEyeAdornment = props => {
+  const { hidden, t, ...rest } = omit(props, 'f')
+
   return (
     <InputAdornment position="end">
       <Button
@@ -11,11 +15,15 @@ const EyeAdornment = ({ hidden, ...props }) => {
         icon={hidden ? 'eye' : 'eye-closed'}
         className="u-ph-half u-mh-0 u-miw-auto"
         type="button"
-        {...props}
+        label={hidden ? t('unlock.show') : t('unlock.hide')}
+        {...rest}
       />
     </InputAdornment>
   )
 }
+
+const EyeAdornment = translate()(DumbEyeAdornment)
+
 const PasswordField = props => {
   const [hidden, setHidden] = useState(true)
 
