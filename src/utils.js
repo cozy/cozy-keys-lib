d45eb7b62a3bb3412a002467f3c46bad2c0a4402
Cyrille Perois <cperois@outlook.com>
2020-01-15 12:24:45 +0100

    feat: Check if extension has been installed before ask for unlock
    We figured than only checking if some ciphers exist is not a strong
    enough condition to know if we should ask the user to unlock the vault
    or not.
    
    To resolve our problems, the stack now adds a
    `extension_installed` attribute on the `io.cozy.settings.bitwarden`
    document when an extension has been connected for the first time to the
    instance. See https://github.com/cozy/cozy-stack/pull/2315.
    
    So we now check this attribute. The flow is now:
    
    * Check if there is some cipher, is yes => the user should unlock
    * If there is no cipher, check if the extension has been installed, if
      yes => the user should unlock
    * Otherwise, skip the unlock step

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const CIPHERS_DOCTYPE = 'com.bitwarden.ciphers'
+const SETTINGS_DOCTYPE = 'io.cozy.settings'
 
 const isForbiddenError = rawError => {
   return rawError.message.match(/code=403/)
@@ -13,13 +14,38 @@ export const checkHasCiphers = async cozyClient => {
     return ciphers.length > 0
   } catch (err) {
     /* eslint-disable no-console */
-    console.error('Error while fetching ciphers:')
     if (isForbiddenError(err)) {
       console.error(
         `Your app must have the GET permission on the ${CIPHERS_DOCTYPE} doctype.`
       )
     } else {
-      console.error(err)
+      console.error(err.message)
+    }
+    /* eslint-enable no-console */
+
+    return false
+  }
+}
+
+export const checkHasInstalledExtension = async cozyClient => {
+  try {
+    const { rows: docs } = await cozyClient
+      .getStackClient()
+      .fetchJSON('GET', `/data/${SETTINGS_DOCTYPE}/_normal_docs`)
+
+    const [bitwardenSettings] = docs.filter(
+      doc => doc._id === 'io.cozy.settings.bitwarden'
+    )
+
+    return bitwardenSettings && bitwardenSettings.extension_installed
+  } catch (err) {
+    /* eslint-disable no-console */
+    if (isForbiddenError(err)) {
+      console.error(
+        `Your app must have the GET permission on the ${SETTINGS_DOCTYPE} doctype.`
+      )
+    } else {
+      console.error(err.message)
     }
     /* eslint-enable no-console */
 
