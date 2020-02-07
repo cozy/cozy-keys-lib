c0c300bbc96c940ffc4f2e686239a396169dc2f6
Patrick Browne <pt.browne@gmail.com>
2020-02-07 13:50:53 +0100

    feat: Add node version without React components
    This is useful when making CLI for libraries using cozy-keys-lib
    like harvest

---
+++ src/index.node.js
@@ -0,0 +1,4 @@
+export { default as WebVaultClient } from './WebVaultClient'
+export * as CozyUtils from './CozyUtils'
+export { default as CipherType } from './CipherType'
+export { default as UriMatchType } from './UriMatchType'
