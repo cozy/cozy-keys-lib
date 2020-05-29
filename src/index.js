3b74cb37ee8ff7ac6e0796669f7d32e3c56576e1
Patrick Browne <pt.browne@gmail.com>
2020-05-29 12:10:15 +0200

    feat: Export FieldType

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as WebVaultClient } from './WebVaultClient'
 export * as CozyUtils from './CozyUtils'
 export { default as CipherType } from './CipherType'
 export { default as UriMatchType } from './UriMatchType'
+export { default as FieldType } from './FieldType'
 
 export {
   VaultContext,
