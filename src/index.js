cozy/cozy-keys-lib

commit cc8f07932ca400afce1d82f3d73c27f6bfe28074

feat: Allow passing client as props in VaultContext
It can be useful to pass an initialized client to VaultContext
in case it must be shared between react components and redux store.
This avoids ending up with several client instance, holding different
states.

src/index.js
@@ -18,3 +18,4 @@ export {
   VaultUnlockProvider,
   withVaultUnlockContext
 } from './components/vaultUnlockContext'
+export { UnlockForm } from './components/UnlockForm'

src/components/VaultContext.jsx
@@ -15,7 +15,7 @@ class VaultProvider extends React.Component {
     super(props)
 
     this.state = {
-      client: null,
+      client: this.props.client,
       locked: true
     }
 
@@ -24,9 +24,7 @@ class VaultProvider extends React.Component {
   }
 
   componentDidMount() {
-    if (this.props.instance) {
-      this.setupClient()
-    }
+    this.setupClient()
   }
 
   componentWillUnmount() {
@@ -49,12 +47,9 @@ class VaultProvider extends React.Component {
 
   setupClient() {
     const unsafeStorage = this.props.unsafeStorage
-
-    const client = getVaultClient(
-      this.props.instance,
-      unsafeStorage,
-      this.props.vaultData
-    )
+    const client =
+      this.props.client ||
+      getVaultClient(this.props.instance, unsafeStorage, this.props.vaultData)
 
     this.setState(
       {
@@ -86,7 +81,8 @@ class VaultProvider extends React.Component {
 }
 
 VaultProvider.propTypes = {
-  instance: PropTypes.string.isRequired,
+  instance: PropTypes.string,
+  client: PropTypes.object,
   unsafeStorage: PropTypes.bool,
   setClient: PropTypes.func
 }

commit 206ee0f5499045b928421a749623be6d1ff48ce4

fix: Remove broken UnlockForm export
Since cc8f07932ca400afce1d82f3d73c27f6bfe28074, the UnlockForm is
exported. However, this was wrongly exported as it is default exported
in the component. Thus, this cannot work and triggers a warning in apps
using the lib such as Drive: `export 'UnlockForm' was not found`.
Consequently, we remove this useless export

src/index.js
@@ -18,4 +18,3 @@ export {
   VaultUnlockProvider,
   withVaultUnlockContext
 } from './components/vaultUnlockContext'
-export { UnlockForm } from './components/UnlockForm'